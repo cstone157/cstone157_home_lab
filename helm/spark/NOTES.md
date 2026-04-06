f99e45d1ff3fc7621c094020c4ae897fcc20ad1c
cstone157 <culver.stone57@gmail.com>
2026-04-06 13:16:45 -0700

    Got the jupyterlab/spark working.  Moving on for the time being.

--- helm/spark/NOTES.md
+++ helm/spark/NOTES.md
@@ -1,48 +1,3 @@
-#
-
-### 1.) Installing the spark operator (https://github.com/kubeflow/spark-operator)
-
-### 2.) Create Namespace
-
-- $ kubectl create namespace spark-apps
-
-### 3.) Create Service Account
-
-- $ kubectl apply -f spark-pi.yaml
-
-### 4.) Create Spark application
-
-- $ kubectl apply -f spark-app.yaml (NOT WORKING)
-
-
-#### Delete the application
-- $ kubectl delete sparkapp spark-pi
-
-
-#
-
-## Deploying using the Spark Operator
-
-## Set up RBAC (Role-Based Access Control)
-
-- $ kubectl create serviceaccount spark
-- $ kubectl create clusterrolebinding spark-role --clusterrole=edit --serviceaccount=default:spark --namespace=default
-
-### Install the operator with helm
-
-- $ helm repo add spark-operator https://kubeflow.github.io/spark-operator
-- $ helm repo update
-- $ helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set webhook.enable=true
-
-### Install a spark application using spark-pi.yaml
-
-- $ kubectl apply -f spark-pi.yaml
-
-### Accessing the UI
-
-- $ kubectl port-forward my-release-spark-operator-webhook-<hash> -n spark-operator 4040:4040
-
-
 ## ==== Script for in the jupyterlab ====
 import os
 from pyspark.sql import SparkSession
@@ -95,3 +50,9 @@ ordered_df = df.orderBy(col("Score").desc())
 # 4. Bring the results back to Jupyter and display them
 print("Ordered Data (Highest Score First):")
 ordered_df.show()
+
+## ==== Close spark session ====
+# Stop the Spark session and release resources
+spark.stop()
+
+print("Spark session closed.")
--- helm/spark/spark.sh
+++ helm/spark/spark.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 
-# This script is used to deploy the Spark Pi example application using Helm.
-# kubectl create serviceaccount spark
-# kubectl create clusterrolebinding spark-role --clusterrole=edit --serviceaccount=default:spark --namespace=default
-# helm repo add spark-operator https://kubeflow.github.io/spark-operator
-# helm repo update
-# helm install my-release spark-operator/spark-operator --namespace spark-operator --create-namespace --set webhook.enable=true
-# echo "Waiting for the Spark Operator to be fully deployed..."
-# sleep 15 # Wait for the Spark Operator to be fully deployed
-# kubectl apply -f spark-pi.yaml
-
-## To uninstall the Spark Operator and the Spark Pi application, you can use the following commands:
-# kubectl delete -f spark-pi.yaml
-# kubectl delete namespace spark-operator
-# kubectl delete clusterrolebinding spark-role
-# kubectl delete serviceaccount spark
-
-
 ## ========================================
 ## Build the Spark Runner Docker image
 ## ========================================
 nerdctl build -t spark-runner:latest -f spark-runner/Dockerfile -namespace=k8s.io ./spark-runner/
 
 ## ========================================
-## Deploy the Spark/JupyterLab application
-## ========================================
 ## Apply the RBAC configuration for Spark
+## ========================================
 kubectl apply -f spark-rbac.yaml
 
 sleep 5
 
+## ========================================
 ## Deploy JupyterLab
+## ========================================
 kubectl apply -f jupyter-deployment.yaml
 
+## ========================================
 ## Get the token for JupyterLab access
+## ========================================
 kubectl logs deployment/jupyterlab | grep "token="
 
+## ========================================
 ## Enable port forwarding to access JupyterLab
+## ========================================
 kubectl port-forward svc/jupyterlab-service 8888:8888
 
-## To uninstall the Spark Operator and the JupyterLab deployment, you can use the following commands:
+## ========================================
+## To uninstall the Spark Operator and the 
+## JupyterLab deployment, you can use the 
+## following commands:
+## ========================================
 # kubectl delete -f jupyter-deployment.yaml
 # kubectl delete -f spark-rbac.yaml
